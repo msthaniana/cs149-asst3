05b31f8959b61f980086e005ea8d82e16b64b9b2
Nicholas Trank <monosaltamonte@gmail.com>
2025-10-28 20:54:30 +0000

    New version that checks per-box rather than per-pixel circles

---
+++ render/cudaRenderer2.h
@@ -0,0 +1,47 @@
+#ifndef __CUDA_RENDERER_H__
+#define __CUDA_RENDERER_H__
+
+#include "circleRenderer.h"
+
+class CudaRenderer : public CircleRenderer {
+ private:
+  Image* image;
+  SceneName sceneName;
+
+  int numCircles;
+  float* position;
+  float* velocity;
+  float* color;
+  float* radius;
+  int boxWidth;
+  int boxHeight;
+
+  float* cudaDevicePosition;
+  float* cudaDeviceVelocity;
+  float* cudaDeviceColor;
+  float* cudaDeviceRadius;
+  float* cudaDeviceImageData;
+  uint8_t* cudaDeviceBoxList;
+ public:
+  CudaRenderer();
+  virtual ~CudaRenderer();
+
+  const Image* getImage();
+
+  void setup();
+
+  void loadScene(SceneName name, int seed = 0);
+
+  void allocOutputImage(int width, int height);
+
+  void clearImage();
+
+  void advanceAnimation();
+
+  void render();
+
+  void shadePixel(int circleIndex, float pixelCenterX, float pixelCenterY,
+                  float px, float py, float pz, float* pixelData);
+};
+
+#endif
